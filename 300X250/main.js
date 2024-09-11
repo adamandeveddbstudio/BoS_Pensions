8ed9eb386eb21ca94fc47a3fbf9c1494a08943c9
AbhijithCSundaran <161324727+adamandeveddbstudio@users.noreply.github.com>
2024-09-11 15:11:46 +0530

    new animation change

--- 300X250/main.js
+++ 300X250/main.js
@@ -7,7 +7,7 @@ init();
 function init() {
   // Set Banner duration timer
   startTime = new Date();
-
+  gsap.registerPlugin(MotionPathPlugin);
   // Set Global Timeline
   tl = new TimelineMax({ onComplete: endTime });
   // tl = gsap.timeline({ repeat: -1, repeatDelay: 3 });
@@ -17,38 +17,89 @@ function init() {
 
 function animate() {
   tl.set(["#main_content"], { autoAlpha: 1, rotationZ: 0.1, force3D: true });
-  tl.set(["#sugar_wrpr"], { x: -10, rotationZ: -70, force3D: true });
+  // tl.set(["#sugar_wrpr"], { x: -10, rotationZ: -70, force3D: true });
   tl.set(["#manageBtn .btn_overlay"], { autoAlpha: 0, scale: 0, force3D: true });
+  tl.set(["#logo"], { autoAlpha: 0, scale: 0.25, force3D: true });
+  tl.set(["#sugar_container"], { autoAlpha: 0, y: "100%", force3D: true });
+  // tl.set(["#cross"], { scale:(0.6,0.8), force3D: true });
+  // tl.set(["#dots"], { scale: 0.8, force3D: true });
 
-  tl.addLabel('frame_1', 0.5)
-    .to(['#h1', '#logo'], 0.5, { autoAlpha: 1, ease: Power2.easeOut }, "frame_1")
+  tl.addLabel('frame_1', 0)
+    .to(['#h1'], 0.5, { autoAlpha: 1, scale: 1, ease: Power2.easeOut }, "frame_1")
+    .to(['#logo'], 0.5, { autoAlpha: 1, scale: 1, ease: Power2.easeOut }, "frame_1")
+    .to(['.l-circle', '.l-cross'], 0.25, { backgroundColor: '#fff', ease: Power2.easeOut }, "frame_1+=0.25")
+    .to(['#dots'], 0.25, { width: 'calc(100% + 30px)', height: 'calc(100% + 20px)', x: -15, y: -10, ease: Power2.ease }, "frame_1+=0.25")
+    .to(['#dots'], 0.25, { width: 'calc(100% - 20px)', height: 'calc(100% - 15px)', x: 10, y: 7.5, ease: Power2.ease }, "frame_1+=0.5")
+    .to(['#dots'], 0.25, { width: '100%', height: '100%', x: 0, y: 0, ease: Power2.ease }, "frame_1+=0.75")
 
   tl.addLabel('frame_2', 2)
-    .to(['#h1', '#logo'], 0.5, { autoAlpha: 0, ease: Power2.easeOut }, "frame_2")
-    .to(['#h2', '#cupU', '#cupL', '#sugar_container', '#upper'], 0.5, { autoAlpha: 1, ease: Power2.easeOut }, "frame_2+=0.5")
-    .to(['#lower'], 0, { autoAlpha: 1, ease: Power2.easeOut }, "frame_2+=1")
-    .to(['#sugar_wrpr'], 2, { x: 0, rotationZ: 70, ease: Power2.ease }, "frame_2+=1.5")
 
-  tl.addLabel('frame_3', 5.5)
-    .to(['#h2', '#sugar_container'], 0.5, { autoAlpha: 0, ease: Power2.easeOut }, "frame_3")
-    .staggerTo(['#wdot1', '#cdot1', '#wdot2', '#cdot2', '#wdot3', '#cdot3', '#wdot4', '#cdot4',], 0.125, { autoAlpha: 1, ease: Power2.easeNone }, 0.125, 'frame_+=0')
-    .to(['#h3', '#combine_container'], 0.5, { autoAlpha: 1, ease: Power2.easeOut }, "frame_3+=0.5")
-    .to(['#combine_container'], 4, { rotationZ: 45, ease: Power2.easeNone }, "frame_3+=2")
+    .to(['#h1'], 0.5, { autoAlpha: 0, ease: Power2.easeOut }, "frame_2")
+    .to(['#cross'], 0.5, { scale: 0, autoAlpha: 0, ease: Power2.easeOut }, "frame_2")
+    .to(['.l-cross'], 0.5, { backgroundColor: '#2ab3d5', autoAlpha: 0, ease: Power2.easeOut }, "frame_2")
+    .to(['#dots'], 0.25, { width: 'calc(100% - 50px)', x: 25, ease: Power2.ease }, "frame_2+=1")
+    .to(['#dots'], 1.5, { rotationZ: 460, ease: Power2.easeNone }, "frame_2+=1")
 
-  tl.addLabel('frame_4', 9.5)
-    .to(['#h3', '#combine_container'], 0.5, { autoAlpha: 0, ease: Power2.easeOut }, "frame_4")
-    .to(['#h4', '#phn', '#manageBtn','#phn-content', '#phnLogo'], 0.5, { autoAlpha: 1, ease: Power2.easeOut }, "frame_4+=0.5")
-    .to(["#manageBtn .btn_overlay"], 0.1, { autoAlpha: 1, ease: Power2.easeOut }, "frame_4")
-    .to(["#manageBtn"], 0.25, { scale: 0.95, ease: Power2.easeOut }, "frame_4+=2")
-    .to(["#manageBtn .btn_overlay"], 0.25, { scale: 1, ease: Power2.easeOut }, "frame_4+=2")
-    .to(["#manageBtn"], 0, { scale: 1, ease: Power2.easeOut }, "frame_4+=2.25")
-    .to(["#manageBtn .btn_overlay"], 0, { autoAlpha: 0, ease: Power2.easeOut }, "frame_4+=2.25")
-    // .fromTo('#manageBtn', .5, { scale: 0.9 }, { scale: 1, ease: Quad.easeOut, force3D: true }, "frame_4+=1")
-    // .to(['#phn-content', '#phnLogo'], 0, { autoAlpha: 1, ease: Power2.easeOut }, "frame_4+=1.5")
 
-  tl.addLabel('frame_5', 13)
-    .to(['#h4', '#phn', '#manageBtn', '#phn-content'], 0.5, { autoAlpha: 0, ease: Power2.easeOut }, "frame_5")
-    .to(['#h5', '#logo', '#cta'], 0.5, { autoAlpha: 1, ease: Power2.easeOut }, "frame_5+=0.5")
+    .to(['#circle-two'], 0, { autoAlpha: 0, ease: Power2.easeNone }, "frame_2+=1.15")
+    .to(['#dcircle-two'], 0, { autoAlpha: 1, ease: Power2.easeNone }, "frame_2+=1.15")
+    .to("#dcircle-two", {
+      left: -14, top: 7,
+      duration: 1, yoyo: false, ease: 'linear', motionPath: { path: "#path", autoRotate: true, alignOrigin: [0, 0] }
+    }, "frame_2+=1.15")
+
+    .to(['#circle-one'], 0, { autoAlpha: 0, ease: Power2.easeNone }, "frame_2+=1.32")
+    .to(['#dcircle-one'], 0, { autoAlpha: 1, ease: Power2.easeNone }, "frame_2+=1.32")
+    .to("#dcircle-one", {
+      left: -14, top: 7,
+      duration: 1, yoyo: false, ease: 'linear', motionPath: { path: "#path", autoRotate: true, alignOrigin: [0, 0] }
+    }, "frame_2+=1.32")
+
+    .to(['#circle-four'], 0, { autoAlpha: 0, ease: Power2.easeNone }, "frame_2+=1.53")
+    .to(['#dcircle-four'], 0, { autoAlpha: 1, ease: Power2.easeNone }, "frame_2+=1.53")
+    .to("#dcircle-four", {
+      left: -14, top: 7,
+      duration: 1, yoyo: false, ease: 'linear', motionPath: { path: "#path", autoRotate: true, alignOrigin: [0, 0] }
+    }, "frame_2+=1.53")
+
+    .to(['#circle-three'], 0, { autoAlpha: 0, ease: Power2.easeNone }, "frame_2+=1.82")
+    .to(['#dcircle-three'], 0, { autoAlpha: 1, ease: Power2.easeNone }, "frame_2+=1.82")
+    .to("#dcircle-three", {
+      left: -14, top: 7,
+      duration: 1, yoyo: false, ease: 'linear', motionPath: { path: "#path", autoRotate: true, alignOrigin: [0, 0] }
+    }, "frame_2+=1.82")
+
+    .to(['#sugar_container'], 0.5, { autoAlpha: 1, y: 0, ease: Power2.easeOut }, "frame_2+=1.9")
+    .to(['#dcircle-two'], 0, { autoAlpha: 0, ease: Power2.easeNone }, "frame_2+=2.15")
+    .to(['#dcircle-one'], 0, { autoAlpha: 0, ease: Power2.easeNone }, "frame_2+=2.32")
+    .to(['#dcircle-four'], 0, { autoAlpha: 0, ease: Power2.easeNone }, "frame_2+=2.53")
+    .to(['#dcircle-three'], 0, { autoAlpha: 0, ease: Power2.easeNone }, "frame_2+=2.82")
+
+
+  // .to(['#h2', '#cupU', '#cupL', '#sugar_container', '#upper'], 0.5, { autoAlpha: 1, ease: Power2.easeOut }, "frame_2+=0.5")
+  // .to(['#lower'], 0, { autoAlpha: 1, ease: Power2.easeOut }, "frame_2+=1")
+  // .to(['#sugar_wrpr'], 2, { x: 0, rotationZ: 70, ease: Power2.ease }, "frame_2+=1.5")
+
+  // tl.addLabel('frame_3', 5.5)
+  //   .to(['#h2', '#sugar_container'], 0.5, { autoAlpha: 0, ease: Power2.easeOut }, "frame_3")
+  //   .staggerTo(['#wdot1', '#cdot1', '#wdot2', '#cdot2', '#wdot3', '#cdot3', '#wdot4', '#cdot4',], 0.125, { autoAlpha: 1, ease: Power2.easeNone }, 0.125, 'frame_+=0')
+  //   .to(['#h3', '#combine_container'], 0.5, { autoAlpha: 1, ease: Power2.easeOut }, "frame_3+=0.5")
+  //   .to(['#combine_container'], 4, { rotationZ: 45, ease: Power2.easeNone }, "frame_3+=2")
+
+  // tl.addLabel('frame_4', 9.5)
+  //   .to(['#h3', '#combine_container'], 0.5, { autoAlpha: 0, ease: Power2.easeOut }, "frame_4")
+  //   .to(['#h4', '#phn', '#manageBtn','#phn-content', '#phnLogo'], 0.5, { autoAlpha: 1, ease: Power2.easeOut }, "frame_4+=0.5")
+  //   .to(["#manageBtn .btn_overlay"], 0.1, { autoAlpha: 1, ease: Power2.easeOut }, "frame_4")
+  //   .to(["#manageBtn"], 0.25, { scale: 0.95, ease: Power2.easeOut }, "frame_4+=2")
+  //   .to(["#manageBtn .btn_overlay"], 0.25, { scale: 1, ease: Power2.easeOut }, "frame_4+=2")
+  //   .to(["#manageBtn"], 0, { scale: 1, ease: Power2.easeOut }, "frame_4+=2.25")
+  //   .to(["#manageBtn .btn_overlay"], 0, { autoAlpha: 0, ease: Power2.easeOut }, "frame_4+=2.25")
+  //   // .fromTo('#manageBtn', .5, { scale: 0.9 }, { scale: 1, ease: Quad.easeOut, force3D: true }, "frame_4+=1")
+  //   // .to(['#phn-content', '#phnLogo'], 0, { autoAlpha: 1, ease: Power2.easeOut }, "frame_4+=1.5")
+
+  // tl.addLabel('frame_5', 13)
+  //   .to(['#h4', '#phn', '#manageBtn', '#phn-content'], 0.5, { autoAlpha: 0, ease: Power2.easeOut }, "frame_5")
+  //   .to(['#h5', '#logo', '#cta'], 0.5, { autoAlpha: 1, ease: Power2.easeOut }, "frame_5+=0.5")
 
 }
 
